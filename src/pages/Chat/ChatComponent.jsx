37dd2943fa7298dc10b98ae76c618c2c98a28efc
Amelia Goodson <ameliajanegoodson@gmail.com>
2025-03-01 22:54:16 -0500

    Update code field with formatting and highlighting

---
+++ src/pages/Chat/ChatComponent.jsx
@@ -0,0 +1,18 @@
+import Button from "../../components/Button";
+
+const ChatComponent = () => {
+  return (
+    <div>
+      <div className="chat-interface h-70 w-full bg-white border-1 mb-4 rounded-md p-4">
+
+        {/* store conversation */}
+
+        test
+      </div>
+      <textarea placeholder="Ask GPT a question"></textarea>
+      <Button ></Button>
+    </div>
+  );
+};
+
+export default ChatComponent;
--- src/pages/Flashcards/useFlashcards.jsx
+++ src/pages/Flashcards/useFlashcards.jsx
@@ -10,13 +10,6 @@ const useFlashcards = () => {
   const [isSaved, setIsSaved] = useState(false);
   const [editMode, setEditMode] = useState(false);
 
-  // Define placeholders
-  const placeholders = {
-    question: "Question",
-    answer: "Answer",
-    category: "Category",
-  };
-
   // EDIT function to switch to edit mode
   // Set initial value of inputs to the original query, response and category
   const editFlashcard = (query, response, category) => {
@@ -28,41 +21,6 @@ const useFlashcards = () => {
     setEditMode(true);
   };
 
-  const saveFlashcard = (query, response, category, user) => {
-    const questionToSave =
-      editedQuestion === "" || editedQuestion === placeholders.question
-        ? query
-        : editedQuestion;
-
-    const answerToSave =
-      editedAnswer === "" || editedAnswer === placeholders.answer
-        ? response
-        : editedAnswer;
-
-    const categoryToUse =
-      editedCategories === "" || editedCategories === placeholders.category
-        ? category
-        : editedCategories;
-
-    // Process categories
-    const processedCategories = processCategories(categoryToUse);
-
-    // ✅ Update React state
-    setEditedQuestion(questionToSave);
-    setEditedAnswer(answerToSave);
-    setEditedCategories(processedCategories);
-    setIsSaved(true);
-    setEditMode(false);
-
-    // ✅ Save to Firestore using a separate service function
-    saveToDB({
-      user,
-      question: questionToSave,
-      answer: answerToSave,
-      category: processedCategories,
-    });
-  };
-
   // Return all the functions and state values the component needs
   return {
     editedQuestion,
@@ -70,14 +28,12 @@ const useFlashcards = () => {
     editedCategories,
     isSaved,
     editMode,
-    placeholders,
     setEditedQuestion,
     setEditedAnswer,
     setEditedCategories,
     setIsSaved,
     setEditMode,
     editFlashcard,
-    saveFlashcard,
   };
 };
 export default useFlashcards;
